9abfe5fefcca87bdb7142a5a04811894337eae39
Ajay <ajayrajputwuy@gmail.com>
2026-04-24 12:07:52 +0530

    the post method is created

--- index.js
+++ index.js
@@ -13,6 +13,39 @@ mongoose.connect("mongodb://localhost:27017/full")
 });
 
 
+// this is the post method API use for make the 
+app.post("/create",async(req,res)=>{
+    try {
+        const{name,phone,email,company}=req.body;
+        if(!name || !phone || !email || !company );
+        return res.status(400).json({
+            message:"please enter all the feilds",
+        });
+        const exist = await User.findOne({name});
+        if (exist){
+            return res.status(409).json({
+                message:"the user is already exist in database",
+                data:exist,
+            });
+        }
+        const userdata = new User({
+            name,
+            phone,
+            email,
+            company
+        });
+        await userdata.save();
+        return res.status(201).json({
+            message:"new user is created",
+            data:exist,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message:"internal server error",
+        });
+    }
+});
+
 // check the server is live 
 app.get("/read",(req,res)=>{
     return res.json("mera server live ho chuka h or mene ye post-man pe kr liye h")
